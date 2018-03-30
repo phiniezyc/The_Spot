43d197fc61b934222302437fe34722269e840357
Chance Phiniezy <phiniezyc@gmail.com>
2018-03-30 00:41:54 -0400

    added delete route which works deleting a comment and also removed some test code in show template

--- controllers/routes/comments.js
+++ controllers/routes/comments.js
@@ -68,6 +68,17 @@ router.put('/:comment_id', (req, res) => {
 });
 
 
+// COMMENT DESTROY ROUTE
+router.delete('/:comment_id', (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.redirect(`/spots/${req.params.id}`);
+    }
+  });
+});
+
 // MIDDLEWARE
 function isLoggedIn(req, res, next) { // Can use this on ANY page we want to restrict
   if (req.isAuthenticated()) {
