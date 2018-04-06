adad334edbf45c01c225ee68db1043d11bdb4c0e
Chance Phiniezy <phiniezyc@gmail.com>
2018-04-06 02:41:31 -0400

    leaned up code.  Commented out helper and just relied on handlebars if statements for flash messages view

--- controllers/routes/index.js
+++ controllers/routes/index.js
@@ -62,7 +62,7 @@ router.post('/login', passport.authenticate('local', {
 
 router.get('/logout', (req, res) => {
   req.logOut();
-  req.flash('success', 'You are now logged out.');
+  req.flash('success', 'You are now logged out');
   res.redirect('/spots');
 });
 
--- helpers/hbsHelpers.js
+++ helpers/hbsHelpers.js
@@ -1,14 +1,14 @@
 
 
-module.exports = {
-  test(success) {
-    console.log(success);
-    // return num1 + num2 + 'helper is connected!';
-    if (success.length > 0) {
-      return "yes it's here";
-    }
-    return 'nope not here';
-  },
+// module.exports = {
+//   test(success) {
+//     // console.log(success);
+//     // // return num1 + num2 + 'helper is connected!';
+//     // if (success.length > 0) {
+//     //   return success.fn(this);
+//     // }
+//     // return 'nope not here';
+//   },
 
 
-};
+// };
