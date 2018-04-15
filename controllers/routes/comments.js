c4cdc803eb23a3ec2f0bccc974b99dcda863f2a4
Chance Phiniezy <phiniezyc@gmail.com>
2018-04-15 02:05:09 -0400

    cleaned up code

--- controllers/routes/comments.js
+++ controllers/routes/comments.js
@@ -5,7 +5,7 @@ const router = express.Router({ mergeParams: true });
 // this merges the params from the spot and comment together
 const Spot = require('../../models/Spots');
 const Comment = require('../../models/Comments');
-const middleware = require('../../middleware'); // index.js is a special name! Don't need to specify the specific file name if it's named index.js. Just require the parent file!
+const middleware = require('../../middleware'); // index.js is a special name. Don't need to specify specific file name if named index.js Just require the parent file!
 
 
 // ========Comment Routes ======================
@@ -37,8 +37,8 @@ router.post('/', middleware.isLoggedIn, (req, res) => { // protects from someone
           comment.author.username = req.user.username;
           comment.save();
 
-          //  spot here refers to the spot returned after finding Spot by id in this post route
           spot.comments.push(comment);
+          // spot here refers to the spot returned after finding Spot by id in this post route
           spot.save();
           req.flash('sucess', 'Successfully added comment');
           res.redirect(`/spots/${spot._id}`);
@@ -59,7 +59,7 @@ router.get('/:comment_id/edit', middleware.checkCommentOwnership, (req, res) => 
       if (err) {
         res.redirect('back');
       } else {
-        // we have access to req.params.id because of the long route defined in server.js
+        // we have access to req.params.id because of long route defined in server.js
         res.render('comments/edit', { spot_id: req.params.id, comment: foundComment });
       }
     });
@@ -90,31 +90,5 @@ router.delete('/:comment_id', middleware.checkCommentOwnership, (req, res) => {
   });
 });
 
-// MIDDLEWARE
-// function isLoggedIn(req, res, next) { // Can use this on ANY page we want to restrict
-//   if (req.isAuthenticated()) {
-//     return next();
-//   }
-//   res.redirect('/login');
-// }
-
-// function checkCommentOwnership(req, res, next) {
-//   if (req.isAuthenticated()) {
-//     Comment.findById(req.params.comment_id, (err, foundComment) => {
-//       if (err) {
-//         res.redirect('back');
-//       } else if (foundComment.author.id.equals(req.user._id)) {
-//         /* must use .equals mongoose method because look the same but one actually object
-//         other a string! foundComment... is a mongoose object and
-//         req.user... a string.  _.id is stored in req.user thanks to passport */
-//         next();
-//       } else {
-//         res.redirect('back');
-//       }
-//     });
-//   } else {
-//     res.redirect('back');
-//   }
-// }
 
 module.exports = router;
