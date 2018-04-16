344222daed56441e441b1f7f9e630c91816376b8
Chance Phiniezy <phiniezyc@gmail.com>
2018-04-16 00:18:46 -0400

    cleaned up code

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 
 const app = express();
@@ -10,30 +9,24 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const expressSession = require('express-session');
 const methodOverride = require('method-override');
+
 const User = require('./models/Users');
-
-
 const commentRoutes = require('./controllers/routes/comments');
 const spotRoutes = require('./controllers/routes/spots');
 const indexRoutes = require('./controllers/routes/index');
-
 const hbsHelpers = require('./helpers/hbsHelpers');
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(`${__dirname}/public`)); // __dirname just says look for it in the full path for the public folder.  It should be unnecessary, but it's just a precaution in case can't find file!
+app.use(express.static(`${__dirname}/public`)); // __dirname means look for it in full path for the public folder.  It should be unnecessary, but a precaution in case can't find file.
 
 app.use(methodOverride('_method'));
-
 app.use(flash());
-
 
 app.engine('handlebars', exphbs({
   defaultLayout: 'main',
   helpers: hbsHelpers,
 }));
-
-app.set('view engine', 'handlebars'); // setting this means we can leave off .handlebars on our handlebars files
+app.set('view engine', 'handlebars'); // setting this means we can leave off .handlebars on our handlebars files because it is understood as a handlebars file.
 
 // PASSPORT CONFIG
 app.use(expressSession({
@@ -50,7 +43,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-  // Custom middleware function so we don't have to add route restriction to every indivual route
+  // Custom middleware function so don't have to add route restriction to every individual route
   res.locals.currentUser = req.user;
   // this is an easy way to pass a variable to all our views. currentUser is available everywhere
   res.locals.error = req.flash('error');
@@ -66,7 +59,7 @@ app.use('/spots/:id/comments', commentRoutes);
 // Wherever we have a param value (like :id) as a prefix to the routes (like commentRoutes),
 // Use mergeParams (in the file) for those routes to get the value of :id passed in to that router.
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise; // Sets mongoose to use the promise API
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/The_Spot');
 
 app.listen(process.env.PORT || 3000, () => {
